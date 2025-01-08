fb1d0e3d5a1b98d9385d9086b4ae6b0bc5dbb7f4
lilong <634916105@qq.com>
2025-01-08 10:30:29 +0800

    update copyright

--- src/OpenVisa/Private/IOTrace.h
+++ src/OpenVisa/Private/IOTrace.h
@@ -1,4 +1,4 @@
-﻿/*********************************************************************************
+/*********************************************************************************
 **                                                                              **
 **  Copyright (C) 2022-2025 LiLong                                              **
 **  This file is part of OpenVisa.                                              **
@@ -16,36 +16,10 @@
 **  You should have received a copy of the GNU Lesser General Public License    **
 **  along with OpenVisa.  If not, see <https://www.gnu.org/licenses/>.          **
 **********************************************************************************/
+
 #pragma once
-
-#include "../Attribute.h"
-
-#include <functional>
-#include <stdexcept>
 
 namespace OpenVisa
 {
-[[noreturn]] inline void visaThrow(const Object::Attribute& attr, std::string_view description)
-{
-    throw std::runtime_error(std::format("{}:{}", attr.deviceName(), description));
-}
 
-template<typename Func>
-requires std::is_invocable_v<Func>
-inline auto visaReThrow(const Object::Attribute& attr, const Func& func)
-{
-    if (attr.deviceName().empty())
-        return std::invoke(func);
-    else
-    {
-        try
-        {
-            return std::invoke(func);
-        }
-        catch (const std::exception& e)
-        {
-            visaThrow(attr, e.what());
-        }
-    }
 }
-} // namespace OpenVisa
